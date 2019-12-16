c6b9b9a5e8b2ab1d04f4e05f32fb3b501b7179d3
ccharris940406 <47110071+ccharris940406@users.noreply.github.com>
2019-12-16 12:50:24 -0500

    Add files via upload

---
+++ airSwitch/airSwitch/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  airSwitch
+//
+//  Created by Carlos Harris on 12/13/19.
+//  Copyright © 2019 Carlos Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    var state = true
+    
+    @IBAction func on_offControl(_ sender: UIButton) {
+        
+        print("the state is \(state)");
+        
+        if state == true {
+            state = false
+            sender.setTitle("Off", for: UIControl.State.normal)
+            sender.backgroundColor = #colorLiteral(red: 0.8898770809, green: 0.5964024663, blue: 0.2226893902, alpha: 1)
+        }
+        else{
+            state = true
+            sender.setTitle("On", for: UIControl.State.normal)
+            sender.backgroundColor = #colorLiteral(red: 0.8479966521, green: 0.8250616789, blue: 0.4791584611, alpha: 1)
+        }
+    }
+}
+
---
+++ airSwitch/airSwitch/rpiConnection.swift
@@ -0,0 +1,13 @@
+//
+//  rpiConnection.swift
+//  airSwitch
+//
+//  Created by Carlos Harris on 12/16/19.
+//  Copyright © 2019 Carlos Harris. All rights reserved.
+//
+
+import Foundation
+
+class rpiConnection{
+    
+}
